8ac319d2905f5ee1f0ba2f41111283769182c978
AWAIS MAYO KING009 <rojxb268@gmail.com>
2026-01-10 23:54:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// Loader removal
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        document.getElementById('loader').style.opacity = '0';
-        setTimeout(() => {
-            document.getElementById('loader').style.display = 'none';
-        }, 500);
-    }, 2000);
-});
-
-// Menu Toggle
 function toggleNav() {
     document.getElementById('sideNav').classList.toggle('active');
 }
