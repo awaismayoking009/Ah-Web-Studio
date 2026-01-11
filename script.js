91b571ecf43720c094040def0313ba104ae5bbc4
AWAIS MAYO KING009 <rojxb268@gmail.com>
2026-01-11 11:48:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Loader removal
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        document.getElementById('loader').style.display = 'none';
+    }, 3000);
+});
+
+// Navigation Toggle
 function toggleNav() {
     document.getElementById('sideNav').classList.toggle('active');
 }
